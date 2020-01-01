6705e90799989820c93828cb3719e0f79b004652
Ben Johnson <ben@bixelcog.com>
2020-01-01 18:12:09 -0500

    Create Bixelcog theme

--- Sources/BixelcogWebsite/main.swift
+++ Sources/BixelcogWebsite/main.swift
@@ -4,15 +4,13 @@ import Plot
 
 struct Bixelcog: Website {
     enum SectionID: String, WebsiteSectionID {
-        // Add the sections that you want your website to contain here:
         case posts
     }
 
     struct ItemMetadata: WebsiteItemMetadata {
-        // Add any site-specific metadata that you want to use here.
+
     }
 
-    // Update these properties to configure your website:
     var url = URL(string: "https://www.bixelcog.com")!
     var name = "Bixelcog LLC"
     var description = "iOS development in Lancaster, PA"
@@ -20,4 +18,4 @@ struct Bixelcog: Website {
     var imagePath: Path? { nil }
 }
 
-try Bixelcog().publish(withTheme: .foundation)
+try Bixelcog().publish(withTheme: .bixelcog)
